b8e7e628cb09f797de84c57cf23278aa6417b947
chuy-zip <andreschuy03@gmail.com>
2023-02-05 19:07:16 -0600

    Added data file and ADT class

---
+++ Hdt3-algoritmos/src/project/comparableInt.java
@@ -0,0 +1,26 @@
+package project;
+
+public class comparableInt implements Comparable<comparableInt>{
+
+	private int IntNumber;
+	
+	public comparableInt(int value) {
+		IntNumber = value;
+	}
+	
+	@Override
+	public int compareTo(comparableInt val) {
+		
+		if (IntNumber > val.IntNumber) {
+			return 1;
+		} 
+		else if(IntNumber > val.IntNumber){
+			return -1;
+		}
+		else {
+			return 0;
+		}
+				
+	}
+
+}
--- Hdt3-algoritmos/src/project/main.java
+++ Hdt3-algoritmos/src/project/main.java
@@ -1,9 +1,25 @@
 package project;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 
+		comparableInt[] ListOfInts;
+		
+		Scanner scan = new Scanner(System.in);
+		String option = "";
+		
+		System.out.println("Elige la opcion que deseas realizar \n"
+						+ "1.Leer el archivo para obtener los numeros \n"
+						+ "2.Crear nuevos numeros y ordenarlos\n"
+						+ "3.Usar Gnome Sort \n"
+						+ "4.Usar Merge Sort \n"
+						+ "5.Usar Quick Sort \n"
+						+ "6.Usar Radix sort \n"
+						+ "7.Usar Bubble Sort");
+		
 
 	}
 
