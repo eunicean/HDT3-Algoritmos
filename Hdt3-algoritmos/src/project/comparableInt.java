d4b793e8dca6a00a85f4c9f929e7072829ea40ac
chuy-zip <andreschuy03@gmail.com>
2023-02-05 20:28:44 -0600

    Numbers can now be read and deleted from file

--- Hdt3-algoritmos/src/project/comparableInt.java
+++ Hdt3-algoritmos/src/project/comparableInt.java
@@ -5,22 +5,30 @@ public class comparableInt implements Comparable<comparableInt>{
 	private int IntNumber;
 	
 	public comparableInt(int value) {
-		IntNumber = value;
+		setIntNumber(value);
 	}
 	
 	@Override
 	public int compareTo(comparableInt val) {
 		
-		if (IntNumber > val.IntNumber) {
+		if (getIntNumber() > val.getIntNumber()) {
 			return 1;
 		} 
-		else if(IntNumber > val.IntNumber){
+		else if(getIntNumber() > val.getIntNumber()){
 			return -1;
 		}
 		else {
 			return 0;
 		}
 				
+	}
+
+	public int getIntNumber() {
+		return IntNumber;
+	}
+
+	public void setIntNumber(int intNumber) {
+		IntNumber = intNumber;
 	}
 
 }
--- Hdt3-algoritmos/src/project/main.java
+++ Hdt3-algoritmos/src/project/main.java
@@ -1,5 +1,10 @@
 package project;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class main {
@@ -9,17 +14,85 @@ public class main {
 		comparableInt[] ListOfInts;
 		
 		Scanner scan = new Scanner(System.in);
-		String option = "";
+		int option = 0;
 		
-		System.out.println("Elige la opcion que deseas realizar \n"
-						+ "1.Leer el archivo para obtener los numeros \n"
-						+ "2.Crear nuevos numeros y ordenarlos\n"
-						+ "3.Usar Gnome Sort \n"
-						+ "4.Usar Merge Sort \n"
-						+ "5.Usar Quick Sort \n"
-						+ "6.Usar Radix sort \n"
-						+ "7.Usar Bubble Sort");
 		
+		while(option != 8) {
+			System.out.println("Elige la opcion que deseas realizar \n"
+					+ "1.Leer el archivo para obtener los numeros \n"
+					+ "2.Crear nuevos numeros y ordenarlos\n"
+					+ "3.Usar Gnome Sort \n"
+					+ "4.Usar Merge Sort \n"
+					+ "5.Usar Quick Sort \n"
+					+ "6.Usar Radix sort \n"
+					+ "7.Usar Bubble Sort \n"
+					+ "8.Salir");
+			option = scan.nextInt();
+			
+			if(option == 1) {
+				
+				System.out.println("Cuantos numeros desea obtener? (10 a 3000)");
+				int QtyOfInts = scan.nextInt();
+
+				ListOfInts = new comparableInt[QtyOfInts];
+				
+				
+				String ScannedNumber = "";
+				try {
+					BufferedReader reader = new BufferedReader(new FileReader("src/project/data.txt"));
+					
+					for (int i = 0; i < ListOfInts.length; i++) {
+						ScannedNumber= reader.readLine();
+						ListOfInts[i] = new comparableInt(Integer.parseInt(ScannedNumber));
+					}
+
+			
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				for (comparableInt comparableInt : ListOfInts) {
+					System.out.println(comparableInt.getIntNumber());
+				}
+				
+			}
+			
+			else if(option == 2) {
+				try {
+					new FileOutputStream("src/project/data.txt").close();
+					
+					
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+			
+			else if(option == 3) {
+				
+			}
+			
+			else if(option == 4) {
+				
+			}
+			
+			else if(option == 5) {
+				
+			}
+			
+			else if(option == 6) {
+				
+			}
+			
+			else if(option == 7) {
+				
+			}
+			
+		}
 
 	}
 
