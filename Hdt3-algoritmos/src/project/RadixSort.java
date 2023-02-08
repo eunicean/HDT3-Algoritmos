72d69573ddcbee08d57d738040643d8ba8276b03
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-02-07 22:32:32 -0600

    RadixSort finished finally mf

--- Hdt3-algoritmos/src/project/RadixSort.java
+++ Hdt3-algoritmos/src/project/RadixSort.java
@@ -6,32 +6,34 @@ public class RadixSort <T extends Comparable<T>>{
 	}
 	
 	//getting the max number in list
-	public T getMaximunNumber(T[] listToFindMaxNumber) {
-		T max = listToFindMaxNumber[0];
+	public int getMaximunNumber(comparableInt[] listToFindMaxNumber) {
+		comparableInt max = listToFindMaxNumber[0];
 		for(int i = 1 ; i < listToFindMaxNumber.length ; i++) {
 			if(listToFindMaxNumber[i].compareTo(max)>=0)
 				max = listToFindMaxNumber[i];
 		}
-		return max;
+		return max.getIntNumber();
 	}
 	
 	//counting sort needed for radix sort
-	public T[] CountingSort(T[] ListToSort, int place) {
-		T maximun = ListToSort[0];
-		int size = ListToSort.length;
-		T[] result = (T[]) new Object[ListToSort.length + 1];
-		int[] count = new int[Integer.parseInt((String) maximun) + 1];
+	public comparableInt[] CountingSort(comparableInt[] listToSort, int place) {
+		comparableInt maximun = listToSort[0];
+		int size = listToSort.length;
+		comparableInt[] result = new comparableInt[listToSort.length + 1];
 		
 		for (int i = 1; i < size; i++) {
-			if (ListToSort[i].compareTo(maximun) >= 0)
-		        maximun = ListToSort[i];
+			if (listToSort[i].compareTo(maximun) >= 0)
+		        maximun = listToSort[i];
 	    }
-		for(int i = 0; i < (int)maximun; i++) 
+		
+		int[] count = new int[maximun.getIntNumber() + 1];
+		
+		for(int i = 0; i <= maximun.getIntNumber(); i++) 
 			count[i] = 0;
 		
 		// calculate count of elements
 		for(int i = 0; i < size; i++) 
-			count[((int)ListToSort[i] / place) % 10]++;
+			count[(listToSort[i].getIntNumber() / place) % 10]++;
 		
 		// calculate cumulative count
 		for (int i = 1; i < 10; i++)
@@ -39,22 +41,22 @@ public class RadixSort <T extends Comparable<T>>{
 		
 		// sorted order
 		for (int i = size - 1; i >= 0; i--) {
-			result[count[((int)ListToSort[i] / place) % 10] - 1] = ListToSort[i];
-			count[((int)ListToSort[i] / place) % 10]--;
+			result[count[(listToSort[i].getIntNumber() / place) % 10] - 1] = listToSort[i];
+			count[(listToSort[i].getIntNumber() / place) % 10]--;
 		}
 		
 		for (int i = 0; i < size; i++)
-			ListToSort[i] = result[i];
+			listToSort[i] = result[i];
 		
-		return ListToSort;
+		return listToSort;
 	}
 	
 	//radix sort (finally)
-	public void AscendigSort(T[] ListToSort, int size) {
-		T max = (getMaximunNumber(ListToSort));
+	public void AscendigSort(comparableInt[] ListToSort, int size) {
+		int max = (getMaximunNumber(ListToSort));
 		
 		// using counting sort to do radix sort
-		for(int place = 1; ((int)max) / place > 0; place *= 10) 
+		for(int place = 1; max / place > 0; place *= 10) 
 			CountingSort(ListToSort, place);
 	}
 }
